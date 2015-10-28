70a0ee28a2fd63456cf4f7dddb67a0b632810550
Stanislau <angelore.main@gmail.com>
2015-10-28 22:29:38 +0300

    Key feature added

--- MailManager/MainWindow.xaml.cs
+++ MailManager/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MailManager
         {
             InitializeComponent();
         }
+
+        private void ExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }

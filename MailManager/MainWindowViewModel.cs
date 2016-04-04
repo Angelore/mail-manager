f90a0cdb8d4e621391e2c263c8b3ecc4f5a88731
Stanislau <angelore.main@gmail.com>
2016-04-04 08:24:08 +0300

    Created a stub for templatemanager

--- MailManager/MainWindowViewModel.cs
+++ MailManager/MainWindowViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailManager.Utility;
 
 namespace MailManager
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel
     {
+        public MainWindowViewModel()
+        {
+            CurrentContentViewModel = new TemplateManager.TemplateManagerViewModel();
+        }
 
+        public BindableBase CurrentContentViewModel { get; set; }
     }
 }
--- MailManager/TemplateManager/TemplateManagerView.xaml.cs
+++ MailManager/TemplateManager/TemplateManagerView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailManager
+namespace MailManager.TemplateManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TemplateManagerView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TemplateManagerView : UserControl
     {
-        public MainWindow()
+        public TemplateManagerView()
         {
             InitializeComponent();
         }

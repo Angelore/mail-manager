aee5fdd9743f79703b763eadc2269d37a32b43cd
Stanislau <angelore.main@gmail.com>
2016-04-05 08:14:53 +0300

    Reimplemented exit and about commands in viewmodel

--- MailManager/MainWindowViewModel.cs
+++ MailManager/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MailManager.Utility;
+using System.Windows;
 
 namespace MailManager
 {
@@ -12,8 +13,31 @@ namespace MailManager
         public MainWindowViewModel()
         {
             CurrentContentViewModel = new TemplateManager.TemplateManagerViewModel();
+
+            ExitMenuCommand = new RelayCommand<MainWindow>(OnExitMenuCommand);
+            AboutMenuCommand = new RelayCommand<MainWindow>(OnAboutMenuCommand);
         }
 
         public BindableBase CurrentContentViewModel { get; set; }
+        
+        public RelayCommand<MainWindow> ExitMenuCommand { get; private set; }
+        public RelayCommand<MainWindow> AboutMenuCommand { get; private set; }
+
+        public void OnExitMenuCommand(MainWindow window)
+        {
+            window.Close();
+        }
+
+        public void OnAboutMenuCommand(MainWindow window)
+        {
+            var childWindow = new Window();
+            childWindow.Content = new AboutView();
+            childWindow.Owner = window;
+            childWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            childWindow.ResizeMode = ResizeMode.NoResize;
+            childWindow.MaxHeight = 300;
+            childWindow.MaxWidth = 500;
+            childWindow.ShowDialog();
+        }
     }
 }

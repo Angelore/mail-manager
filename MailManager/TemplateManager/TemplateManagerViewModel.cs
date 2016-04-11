fab897229e92af090149759520287a7e77bbab07
Stanislau <angelore.main@gmail.com>
2016-04-11 08:33:41 +0300

    Added some code for the selection of month

--- MailManager/TemplateManager/TemplateManagerViewModel.cs
+++ MailManager/TemplateManager/TemplateManagerViewModel.cs
@@ -4,11 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MailManager.Utility;
+using System.Globalization;
 
 namespace MailManager.TemplateManager
 {
     public class TemplateManagerViewModel: BindableBase
     {
 
+        public TemplateManagerViewModel()
+        {
+            MonthNames = GetMonthNames();
+        }
+
+        private string[] _monthNames;
+        public string[] MonthNames
+        {
+            get
+            {
+                return _monthNames;
+            }
+            set
+            {
+                SetProperty(ref _monthNames, value);
+            }
+        }
+
+        private int _selectedMonth;
+        public int SelectedMonth
+        {
+            get
+            {
+                return _selectedMonth;
+            }
+            set
+            {
+                SetProperty(ref _selectedMonth, value);
+            }
+        }
+
+        /// <summary>
+        /// Returns an array of month names for specified culture.
+        /// If nothing is passed, defaults to current culture. Will contain 13 entries, the last one being an empty string for the cultures that use only 12 months.
+        /// </summary>
+        /// <param name="cultureInfo">A CultureInfo object to be used for getting names</param>
+        /// <returns></returns>
+        public string[] GetMonthNames(CultureInfo cultureInfo = null)
+        {
+            var culture = cultureInfo ?? CultureInfo.CurrentCulture;
+            return (culture).DateTimeFormat.MonthNames;
+        }
     }
 }
